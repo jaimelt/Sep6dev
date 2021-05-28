03b64001f9b225bedbb219afa9c722d9e63a27a1
Florin Leonard Bordei <43433262+florinbrd@users.noreply.github.com>
2021-05-28 20:03:21 +0200

    gdfg

--- Application/aspnet-core-dotnet-core/Pages/MyList.cshtml.cs
+++ Application/aspnet-core-dotnet-core/Pages/MyList.cshtml.cs
@@ -7,12 +7,7 @@ namespace aspnet_core_dotnet_core.Pages
 {
     public class MyList : PageModel
     {
-        public LoginCredentials loginCredentials;
-
-        public MyList(LoginCredentials loginCredentials)
-        {
-            this.loginCredentials = loginCredentials; 
-        }
+       
         public void OnGet([FromRoute] int id )
         {
             Console.WriteLine(id);
